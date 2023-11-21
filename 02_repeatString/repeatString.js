1cda1872e15fae37121bef9f6483228243e73339
Rafael Ribeiro <stillrafael@pop-os.localdomain>
2023-11-21 16:22:35 -0300

    finished exercise 02 odinproject javascript

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
-};
-
+const repeatString = function(string, num) {
+    let result
+    if (num <= -1){
+        result = 'ERROR'
+        return result
+    }
+    else if (num >=0) {
+    result = string.repeat(num)
+    return result
+    }
+    }
+repeatString
 // Do not edit below this line
 module.exports = repeatString;
