be7795b34ba062762784d97e467e583fc24dabfb
Rafael Ribeiro <stillrafael@pop-os.localdomain>
2023-11-22 15:28:38 -0300

    finished third test, fixing fourth

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,23 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(objectSequence, ...toRemove) {
+    let where
+    let counter = 0
+    if (toRemove.length > 1) {
+       while (toRemove.length > counter) {
+      
+        where = objectSequence.indexOf(toRemove[counter])
+        if (where == -1){
+            break
+        }
+        objectSequence.splice(where, 1)
+        counter++
+       }
+    }
+    else {
+        where = objectSequence.indexOf(toRemove[0])
+        objectSequence.splice(where, 1) 
+    }
+    return objectSequence
+    
 };
 
 // Do not edit below this line
