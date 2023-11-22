49f4b66be3a5a111fedb36769d7caf9dd0bdfbf1
Rafael Ribeiro <stillrafael@pop-os.localdomain>
2023-11-22 17:57:33 -0300

    finished 05_sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,11 +1,30 @@
 const sumAll = function(firstNumber, secondNumber) {
-let iteration = secondNumber - firstNumber
-let holdingValue = firstNumber + 1
-for (let i of Array(iteration).keys()) {
-    firstNumber +=holdingValue
-    holdingValue++
+let iteration = Math.abs(secondNumber - firstNumber)
+let holdingValue
+let result
+if (firstNumber < 0 || 
+    secondNumber < 0 ||
+    typeof(firstNumber) !== "number" ||
+    typeof(secondNumber) !== "number") {
+    result = "ERROR"
 }
-return firstNumber
+else {
+    if (firstNumber > secondNumber) {
+        holdingValue = secondNumber + 1
+        result = secondNumber
+    }
+    
+    else {
+        holdingValue = firstNumber + 1
+        result = firstNumber
+    }
+    for (let i of Array(iteration).keys()) {
+        result +=holdingValue
+        holdingValue++
+    }
+}
+
+return result
 };
 
 // Do not edit below this line
