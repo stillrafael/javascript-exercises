e26c2b4c55641ed2d7969a061b91deb58a446f71
Rafael Ribeiro <stillrafael@pop-os.localdomain>
2023-11-22 17:19:28 -0300

    finishes first two testes from 05_sumAll
    saving working for the first two tests, before having to tweak the whole algorithm to compare a simple difference

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(firstNumber, secondNumber) {
+let iteration = secondNumber - firstNumber
+let holdingValue = firstNumber + 1
+for (let i of Array(iteration).keys()) {
+    firstNumber +=holdingValue
+    holdingValue++
+}
+return firstNumber
 };
 
 // Do not edit below this line
