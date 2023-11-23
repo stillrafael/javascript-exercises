32e127354b71b92f3cfcfb1c59db89f0430de2c6
Rafael Ribeiro <stillrafael@pop-os.localdomain>
2023-11-23 18:01:08 -0300

    finished first test of 09_palindrome
    it gets kinda complicated right now, so well save this working state to come back to

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (userPhrase) {
+    let reversed = userPhrase.split('').reverse().join('')
+    if (reversed == userPhrase) {
+        return true
+    }
+    else {
+        return false
+    }
 };
 
 // Do not edit below this line
