eaa6c8793a4265f391d646e580f041ba9e171269
Rafael Ribeiro <stillrafael@pop-os.localdomain>
2023-11-23 15:49:47 -0300

    finished 07_tempConversion
    idk how I got a little stuck here, merely by typing the wrong formula.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const toCelsius = 5/9
+const toFarh = 9/5
+let result
+const convertToCelsius = function(degree) {
+  result = ((degree - 32 ) * toCelsius)
+return Number(result.toFixed(1))
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degree) {
+  result = ((degree * toFarh) + 32)
+  return Number(result.toFixed(1))
 };
 
 // Do not edit below this line
