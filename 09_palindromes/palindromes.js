4e351ce769e27982cbc47fa9fd310d9a1023bbc1
Rafael Ribeiro <stillrafael@pop-os.localdomain>
2023-11-23 19:47:49 -0300

    finished 09_palindrome
    got it done by using a regular expression to exclude spaces and punctuation, then reversing and comparing to see if they're the same

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
 const palindromes = function (userPhrase) {
     let reversed = userPhrase.split('').reverse().join('')
+    reversed = reversed.replace(/[!.,\s]/g, '').toLowerCase()
+    userPhrase = userPhrase.replace(/[!.,\s]/g, '').toLowerCase()
     if (reversed == userPhrase) {
         return true
     }
