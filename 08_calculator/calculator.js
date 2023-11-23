5f0967356ac764b8663c42890a8f0dc9a7b1a1b0
Rafael Ribeiro <stillrafael@pop-os.localdomain>
2023-11-23 17:34:08 -0300

    finished 08_calculator
    and learned about recursive function by accident.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+let result
+const add = function(firstNumber, secondNumber) {
+	result = firstNumber + secondNumber
+  return result
 };
 
-const subtract = function() {
-	
+const subtract = function(firstNumber, secondNumber) {
+	result = firstNumber - secondNumber
+  return result
 };
 
-const sum = function() {
-	
+const sum = function(sumAll) {
+  if (sumAll == false) {
+    result = 0
+  }
+  else {
+    result = sumAll.reduce((total, amount) => total + amount)
+  }  
+  return result
 };
 
-const multiply = function() {
-
+const multiply = function(multiply) {
+  result = multiply.reduce((total, amount) => total * amount)
+return result
 };
 
-const power = function() {
-	
+const power = function(firstNumber, secondNumber) {
+  result = firstNumber ** secondNumber
+  return result
 };
 
-const factorial = function() {
-	
+const factorial = function(factoralNumber) {
+  if (factoralNumber <= 1) {
+    return 1
+  }
+  else {
+    result = factoralNumber * factorial(factoralNumber - 1)
+    return result
+  }
 };
 
 // Do not edit below this line
