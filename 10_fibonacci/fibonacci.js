67ae9cba8c10337836a10a14772512f4bdd48c5b
Rafael Ribeiro <stillrafael@pop-os.localdomain>
2023-11-24 14:45:42 -0300

    finished 10_fibonacci
    found a beautiful recursive way to answer this exercise.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+if (number == 1) {
+    return 1;
+}
+if (number == 2) {
+    return 1;
+}
+if (number < 1)
+{
+    return "OOPS"
+}
+return fibonacci(number -1) + fibonacci(number -2);
 };
 
 // Do not edit below this line
